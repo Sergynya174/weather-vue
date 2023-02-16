8b4feeba1fa5df2b8106da9d2567d4956b66d6e6
Sergynya174 <Sergynya174@yandex.ru>
2023-02-16 20:06:36 +0300

    deploy

--- vue.config.js
+++ vue.config.js
@@ -3,3 +3,7 @@ const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === "production" ? "/my-project/" : "/",
+};
