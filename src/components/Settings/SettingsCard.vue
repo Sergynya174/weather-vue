e7a8c0b708720d6dc537f602e74f2723c94a3147
Sergynya174 <Sergynya174@yandex.ru>
2023-02-20 18:45:21 +0300

    поправил баги

--- src/components/Settings/SettingsCard.vue
+++ src/components/Settings/SettingsCard.vue
@@ -2,9 +2,16 @@
     <div v-for="location in $store.state.locations" v-if="$store.state.location !== null" class="flex bg-slate-200 min-w-lg h-24 items-center justify-around">
         <button class="bg-[url('@/assets/burger-menu.png')] w-4 h-4 bg-no-repeat bg-contain"></button>
         <h2 class="mb-2 text-3xl font-thin">{{ location?.name }}</h2>
-        <button @click="$store.commit('deleteWeather', location.id)" class="bg-[url('@/assets/bin.png')] w-4 h-4 bg-no-repeat bg-contain"></button>
+        <button @click="handleClickDelete" class="bg-[url('@/assets/bin.png')] w-4 h-4 bg-no-repeat bg-contain"></button>
     </div>
 </template>
 
 <script>
+    export default {
+            methods: {
+                handleClickDelete(){
+                    this.$store.commit('deleteWeather', this.$store.state.location.id)
+                }
+            }
+        }
 </script>
--- src/components/Settings/SettingsContainer.vue
+++ src/components/Settings/SettingsContainer.vue
@@ -1,14 +1,14 @@
 <template>
     <div class="p-5">
         <h1 class="text-5xl">{{ title }}</h1>
-        <div class="grid grid-cols-4 gap-4 my-10">
+        <div class="grid xl:grid-cols-4 lg:grid-cols-3 sm:grid-cols-2 gap-4 my-5">
             <SettingsCard />
         </div>
         <div class="flex">
             <p class="mr-10 text-2xl">Добавить местоположение:</p>
             <div class="flex items-center border-b-2 border-solid w-2/4">
                 <input class="w-full ml-3 outline-none" v-model="city" />
-                <button @click="handleClickDelete" class="bg-[url('@/assets/add.png')] w-4 h-4 bg-no-repeat bg-contain"></button>
+                <button @click="handleClickWeater" class="bg-[url('@/assets/add.png')] w-4 h-4 bg-no-repeat bg-contain"></button>
             </div>
         </div>
     </div>
@@ -28,7 +28,7 @@
             SettingsCard
         },
         methods: {
-            handleClickDelete(){
+            handleClickWeater(){
                 if(this.city !== ''){
                     this.$store.commit('getCurrentWeather', this.city)
                 }
