734330911b06f51dac2eba90758da5b6230838a3
Sergynya174 <Sergynya174@yandex.ru>
2023-09-09 00:12:43 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/weather-vue",
+  base: "/weather-vue/",
   plugins: [vue()],
   resolve: {
     alias: {
