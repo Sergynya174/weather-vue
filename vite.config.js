bbad5528b612b19f253a1b022514a55343b74c84
Sergynya174 <Sergynya174@yandex.ru>
2023-09-08 20:19:34 +0300

    gh-pages

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,9 @@ import { fileURLToPath, URL } from "node:url";
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
-const base = process.env.NODE_ENV === "production" ? "/vue-weather/" : "/";
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  buse: "/vue-weather",
+  base: "/vue-weather",
   plugins: [vue()],
   resolve: {
     alias: {
